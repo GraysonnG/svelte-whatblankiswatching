0fad43073b94ca39453efab8216f7db562c116ed
Grayson Gullion <xinflux@gmail.com>
2021-02-16 09:52:12 -0500

    attempt to adjust for anime that come out up to 2 weeks early

--- src/utils/datehelper.ts
+++ src/utils/datehelper.ts
@@ -1,17 +1,18 @@
-import type { AnimeInfo } from "../types/kitsuResponse"
+import type { AnimeInfo } from "../types/kitsuResponse";
+import { add } from "date-fns";
 
 export const getAnimeDate = (anime: AnimeInfo) => {
-  return new Date(anime.attributes.startDate.replace(/-/g, "/"))
-}
+  return add(new Date(anime.attributes.startDate.replace(/-/g, "/")), {
+    days: 14,
+  });
+};
 
 export const getUniqueYears = (list: AnimeInfo[]) => {
-  return [
-    ...new Set(
-      list.map((info) => info.animeDate.getFullYear())
-    ),
-  ].sort((a, b) => {
-    if (a > b) return -1
-    if (b > a) return 1
-    return 0
-  });
-}
+  return [...new Set(list.map((info) => info.animeDate.getFullYear()))].sort(
+    (a, b) => {
+      if (a > b) return -1;
+      if (b > a) return 1;
+      return 0;
+    }
+  );
+};
