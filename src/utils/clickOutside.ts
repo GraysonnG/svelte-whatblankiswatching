5b004fb6a3e44ca8fe41cada9e95bfa3d43c4893
Grayson Gullion <xinflux@gmail.com>
2021-02-24 16:26:35 -0500

    fix search bar bug

---
+++ src/utils/clickOutside.ts
@@ -0,0 +1,21 @@
+export const clickOutside = (node: HTMLElement) => {
+  const handleClick = (event: MouseEvent) => {
+    if (
+      node &&
+      !node.contains(event.target as Node) &&
+      !event.defaultPrevented
+    ) {
+      node.dispatchEvent(
+        new CustomEvent<HTMLElement>("click_outside", { detail: node })
+      );
+    }
+  };
+
+  document.addEventListener("click", handleClick, true);
+
+  return {
+    destroy() {
+      document.removeEventListener("click", handleClick, true);
+    },
+  };
+};
