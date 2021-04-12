1d51452687ac74a57942108bd0c5816685aa8e2f
GraysonnG <xinflux@gmail.com>
2021-04-12 14:52:08 -0400

    remove comments

--- rollup.config.js
+++ rollup.config.js
@@ -8,11 +8,6 @@ import typescript from "@rollup/plugin-typescript";
 import css from "rollup-plugin-css-only";
 import graphql from "@rollup/plugin-graphql";
 
-// import { compile } from 'svelte/compiler';
-// const result = comiple(source, {
-
-// })
-
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
@@ -52,24 +47,14 @@ export default {
     svelte({
       preprocess: sveltePreprocess({ sourceMap: !production }),
       compilerOptions: {
-        // custom css hash so I appear professional
         cssHash: ({ hash, css }) => {
           return `wbiw-${hash(css)}`;
         },
-        // enable run-time checks when not in production
         dev: !production,
       },
     }),
     graphql(),
-    // we'll extract any component CSS out into
-    // a separate file - better for performance
     css({ output: "bundle.css" }),
-
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration -
-    // consult the documentation for details:
-    // https://github.com/rollup/plugins/tree/master/packages/commonjs
     resolve({
       browser: true,
       dedupe: ["svelte"],
@@ -79,17 +64,8 @@ export default {
       sourceMap: !production,
       inlineSources: !production,
     }),
-
-    // In dev mode, call `npm run start` once
-    // the bundle has been generated
     !production && serve(),
-
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
     !production && livereload("public"),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
     production && terser(),
   ],
   watch: {
